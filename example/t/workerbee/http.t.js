69b7576e1ae5b8c0320e6491bc576c2d52335b02
Cory Kaufman-Schofield <cory@corykaufman.com>
2014-10-06 13:05:03 -0400

    Added request for inlet readme on GitHub

--- example/t/workerbee/http.t.js
+++ example/t/workerbee/http.t.js
@@ -7,20 +7,25 @@ var proof = require('proof'),
     UserAgent = require('inlet/http/ua'),
     WorkerBee = require('../../index.js')
 
-proof(1, cadence(function(async, assert) {
+proof(3, cadence(function(async, assert) {
     var binder = new Binder('http://127.0.0.1:3000'),
-        workerbee = new WorkerBee(binder),
+        bee = new WorkerBee(binder),
         bouquet = new Bouquet(),
         ua = new UserAgent()
 
     logger.hush = true
 
     async(function() {
-        bouquet.start(workerbee, async())
+        bouquet.start(bee, async())
+    }, function() {
+        ua.fetch(bee.binder, { url: '/' }, async())
     }, function(body) {
-        ua.fetch(workerbee.binder, { url: '/' }, async())
-    }, function(body) {
-        assert(body.toString(), 'Hello from WorkerBee', 'root')
+        assert(body.toString(), 'Hello from WorkerBee', 'workerbee root endpoint')
+
+        ua.fetch({ url: 'https://api.github.com/repos/bigeasy/inlet/readme', headers: { 'User-Agent': 'WorkerBee' } }, async())
+    }, function(body, response) {
+        assert(response.statusCode, 200, 'readme response status')
+        assert(body.name, 'README.md', 'readme name')
     }, function() {
       bouquet.stop(async())
     })
