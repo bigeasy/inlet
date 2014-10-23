ba053127cfe47f23e41efe4aa539e78ff9e7b508
Marshall Rose <mrose17@gmail.com>
2014-10-22 17:24:28 -0700

    [UA] temporary fix for Greenwave server

--- http/ua.js
+++ http/ua.js
@@ -160,7 +160,10 @@ UserAgent.prototype.fetch = cadence(function (step) {
             }, function (body) {
                 var parsed = body
                 var display = null
-                var type = typer.parse(response.headers['content-type'] || 'application/octet-stream')
+// Greenwave server returns "Content-Type:: application/json"
+                var ct = response.headers['content-type']
+                if ((!!ct) && (ct.indexOf(':') === 0))ct = ct.substr(1).trim()
+                var type = typer.parse(ct || 'application/octet-stream')
                 switch (type.type + '/' + type.subtype) {
                 case 'application/json':
                     display = parsed = JSON.parse(body)
