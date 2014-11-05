253d3e7b95b0884f3db69a661d90a8014a6bcfef
Alan Gutierrez <alan@prettyrobots.com>
2014-11-05 11:26:14 -0600

    Remove temporary logging, add global context.

--- monitor/logger.js
+++ monitor/logger.js
@@ -142,11 +142,6 @@ function log (context, level, vargs) {
     })
     if (!object) return
 
-// temporary, until @bigeasy can take a look...
-console.log(JSON.stringify(object, function (key, value) {
-    if (key == 'tls') return true
-    return value
-}))
     queue.entries.push(JSON.stringify(object, function (key, value) {
         if (key == 'tls') return true
         return value
@@ -191,3 +186,5 @@ module.exports.filter = function(contexts, name, filter) {
 
     return module.exports
 }
+
+module.exports.context = {}
