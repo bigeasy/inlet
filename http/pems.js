27c228d3ddaa33f678dee6a6012184bf59a062b1
Alan Gutierrez <alan@prettyrobots.com>
2015-07-30 12:16:36 -0500

    Remove old `http` directory.

--- http/pems.js
+++
@@ -1,6 +0,0 @@
-var path = require('path'), fs = require('fs')
-module.exports = {
-    cert: fs.readFileSync(path.join(__dirname, '../t/fixtures/certs/agent1-cert.pem')),
-    key: fs.readFileSync(path.join(__dirname, '../t/fixtures/certs/agent1-key.pem')),
-    ca: fs.readFileSync(path.join(__dirname, '../t/fixtures/certs/ca1-cert.pem'))
-}
